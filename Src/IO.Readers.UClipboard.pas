a97d281d7c2f50304f322009c579b81623642d13
DelphiDabbler <git@delphidabbler.com>
2012-03-04 20:38:25 +0000

    Added MPL v2 boilerplate comments.

--- Src/IO.Readers.UClipboard.pas
+++ Src/IO.Readers.UClipboard.pas
@@ -1,3 +1,17 @@
+{
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/
+ *
+ * Copyright (C) 2012, Peter Johnson (www.delphidabbler.com).
+ *
+ * $Rev$
+ * $Date$
+ *
+ * Provides a class that can read input data from the clipboard.
+}
+
+
 unit IO.Readers.UClipboard;
 
 interface
