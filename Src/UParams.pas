2c087fe1001399fe7a11ed3a0796699e22878763
DelphiDabbler <git@delphidabbler.com>
2012-03-10 23:57:33 +0000

    Improved command parsing: toggle or switch commands handled better with more informative error messages. Command encapsulated in a record with methods.

--- Src/UParams.pas
+++ Src/UParams.pas
@@ -84,26 +84,58 @@ type
     Ids representing each valid command line comamnd.
   }
   TCommandId = (
-    siInputClipboard, // read input from clipboard
-    siOutputClipboard, // write output to clipboard
-    siOutputFile, // write output to a file
-    siOuputEncoding, // use specified encoding for output
-    siOutputDocType,  // type of document to be output
-    siFragment, // write output as XHTML document fragment
-    siHideCSS, // hide or shows embedded CSS in comments
-    siHideCSSYes, // hides embedded CSS in comments
-    siHideCSSNo,  // does not hide CSS in comments
-    siEmbedCSS, // embed css from a file
-    siLinkCSS, // link to external CSS file
-    siLanguage, // specify output language
-    siTitle, // document title
-    siBranding, // determines whether branding written to code fragments
-    siBrandingYes,  // forces branding on
-    siBrandingNo, // forces branding off
-    siHelp, // display help
-    siQuiet // don't display any output to console
-    );
+    siInputClipboard,   // read input from clipboard
+    siOutputClipboard,  // write output to clipboard
+    siOutputFile,       // write output to a file
+    siOuputEncoding,    // use specified encoding for output
+    siOutputDocType,    // type of document to be output
+    siFragment,         // write output as XHTML document fragment (legacy)
+    siHideCSS,          // hide or shows embedded CSS in comments
+    siForceHideCSS,     // hide embedded CSS in comments (legacy)
+    siEmbedCSS,         // embed css from a file
+    siLinkCSS,          // link to external CSS file
+    siLanguage,         // specify output language
+    siTitle,            // document title
+    siBranding,         // determines whether branding written to code fragments
+    siHelp,             // display help
+    siQuiet             // don't display any output to console
+  );
 
+type
+  TCommand = record
+  strict private
+    const
+      TrueSwitchChars = ['+', '1'];
+      FalseSwitchChars = ['-', '0'];
+      SwitchChars = TrueSwitchChars + FalseSwitchChars;
+  strict private
+    var
+      fCommand: string;
+    function GetName: string;
+    // Validates command syntax, not semantics
+    class procedure ValidateCommand(const Cmd: string); static;
+  public
+    constructor Create(const Cmd: string);
+    // Complete command, including any switches. Same as Name property unless
+    // command is a switch
+    property Command: string read fCommand;
+    // Name of command without any switch values. Same as Command property
+    // unless command is a switch
+    property Name: string read GetName;
+    // Checks if command is a short form command
+    function IsShortForm: Boolean; overload;
+    class function IsShortForm(const Cmd: string): Boolean; overload; static;
+    // Checks if command is a switch
+    function IsSwitch: Boolean;
+    // Value of switch. Error if command not switch
+    function SwitchValue: Boolean;
+    // Checks if given string is a valid command
+    class function IsCommand(const S: string): Boolean; static;
+    class operator Implicit(const S: string): TCommand;
+    class operator Implicit(const Cmd: TCommand): string;
+  end;
+
+type
   {
     TParams:
     Class that parses command line and stores settings according to commands
@@ -114,6 +146,7 @@ type
     var
       fParamQueue: TQueue<string>; // Queue of parameters to be processed
       fCmdLookup: TDictionary<string, TCommandId>;
+      fSwitchCmdLookup: TList<string>;
       fEncodingLookup: TDictionary<string, TOutputEncodingId>;
       fDocTypeLookup: TDictionary<string, TDocType>;
       fBooleanLookup: TDictionary<string, Boolean>;
@@ -121,10 +154,10 @@ type
     procedure PopulateCommandQueue;
     procedure ParseCommand;
     procedure ParseFileName;
-    function GetStringParameter(const Cmd: string): string;
-    function GetBooleanParameter(const Cmd: string): Boolean;
-    function GetEncodingParameter(const Cmd: string): TOutputEncodingId;
-    function GetDocTypeParameter(const Cmd: string): TDocType;
+    function GetStringParameter(const Cmd: TCommand): string;
+    function GetBooleanParameter(const Cmd: TCommand): Boolean;
+    function GetEncodingParameter(const Cmd: TCommand): TOutputEncodingId;
+    function GetDocTypeParameter(const Cmd: TCommand): TDocType;
   public
     constructor Create(const Config: TConfig);
     { Class constructor. Initialises object.
@@ -143,7 +176,7 @@ implementation
 
 uses
   // Delphi
-  StrUtils, SysUtils,
+  StrUtils, SysUtils, Character,
   // Project
   UComparers;
 
@@ -158,13 +191,15 @@ begin
   inherited Create;
   fConfig := Config;
   fParamQueue := TQueue<string>.Create;
-  // create lookup table for commands: case sensitive
+  // lookup table for commands: case sensitive
   fCmdLookup := TDictionary<string,TCommandId>.Create(
     TStringEqualityComparer.Create
   );
   with fCmdLookup do
   begin
     // short forms
+    Add('-b', siBranding);
+    Add('-c', siHideCSS);
     Add('-d', siOutputDocType);
     Add('-e', siOuputEncoding);
     Add('-h', siHelp);
@@ -176,11 +211,6 @@ begin
     Add('-s', siEmbedCSS);
     Add('-t', siTitle);
     Add('-w', siOutputClipboard);
-    // boolean short forms
-    Add('-b0', siBrandingNo);
-    Add('-b1', siBrandingYes);
-    Add('-c0', siHideCSSNo);
-    Add('-c1', siHideCSSYes);
     // long forms
     Add('--doc-type', siOutputDocType);
     Add('--embed-css', siEmbedCSS);
@@ -197,9 +227,16 @@ begin
     Add('--title', siTitle);
     // commands kept for backwards compatibility with v1.x
     Add('-frag', siFragment);
-    Add('-hidecss', siHideCSSYes);
+    Add('-hidecss', siForceHideCSS);
     Add('-rc', siInputClipboard);
     Add('-wc', siOutputClipboard);
+  end;
+  // lookup table for short form commands that are switches
+  fSwitchCmdLookup := TList<string>.Create;
+  with fSwitchCmdLookup do
+  begin
+    Add('-b');
+    Add('-c');
   end;
   // lookup table for --encoding command values: case insensitive
   fEncodingLookup := TDictionary<string,TOutputEncodingId>.Create(
@@ -248,14 +285,16 @@ destructor TParams.Destroy;
 { Class destructor. Tears down object.
   }
 begin
+  fBooleanLookup.Free;
   fDocTypeLookup.Free;
   fEncodingLookup.Free;
+  fSwitchCmdLookup.Free;
   fCmdLookup.Free;
   fParamQueue.Free;
   inherited;
 end;
 
-function TParams.GetBooleanParameter(const Cmd: string): Boolean;
+function TParams.GetBooleanParameter(const Cmd: TCommand): Boolean;
 var
   Param: string;
 resourcestring
@@ -267,7 +306,7 @@ begin
   Result := fBooleanLookup[Param];
 end;
 
-function TParams.GetDocTypeParameter(const Cmd: string): TDocType;
+function TParams.GetDocTypeParameter(const Cmd: TCommand): TDocType;
 var
   Param: string;
 resourcestring
@@ -279,7 +318,7 @@ begin
   Result := fDocTypeLookup[Param];
 end;
 
-function TParams.GetEncodingParameter(const Cmd: string): TOutputEncodingId;
+function TParams.GetEncodingParameter(const Cmd: TCommand): TOutputEncodingId;
 var
   Param: string;
 resourcestring
@@ -291,7 +330,7 @@ begin
   Result := fEncodingLookup[Param];
 end;
 
-function TParams.GetStringParameter(const Cmd: string): string;
+function TParams.GetStringParameter(const Cmd: TCommand): string;
 resourcestring
   sNoParam = 'Parameter for %s missing.';
 begin
@@ -300,7 +339,7 @@ begin
   else
     Result := fParamQueue.Peek;
   if (Result = '') or AnsiStartsStr('-', Result) then
-    raise Exception.CreateFmt(sNoParam, [Cmd]);
+    raise Exception.CreateFmt(sNoParam, [Cmd.Name]);
 end;
 
 procedure TParams.Parse;
@@ -323,15 +362,22 @@ end;
 procedure TParams.ParseCommand;
 resourcestring
   // Error messages
-  sBadCommand = 'Invalid command "%s"';
+  sBadCommand = 'Unrecognised command "%s"';
+  sCantBeSwitch = '%s cannot be a switch command';
+  sMustBeSwitch = '%s must be a switch command: append "+" or "-"';
 var
-  Command: string;
+  Command: TCommand;
   CommandId: TCommandId;
 begin
   Command := fParamQueue.Dequeue;
-  if not fCmdLookup.ContainsKey(Command) then
-    raise Exception.CreateFmt(sBadCommand, [Command]);
-  CommandId := fCmdLookup[Command];
+  if not fCmdLookup.ContainsKey(Command.Name) then
+    raise Exception.CreateFmt(sBadCommand, [Command.Name]);
+  CommandId := fCmdLookup[Command.Name];
+
+  if Command.IsSwitch and not fSwitchCmdLookup.Contains(Command.Name) then
+    raise Exception.CreateFmt(sCantBeSwitch, [Command.Name]);
+  if not Command.IsSwitch and fSwitchCmdLookup.Contains(Command.Name) then
+    raise Exception.CreateFmt(sMustBeSwitch, [Command.Name]);
 
   case CommandId of
     siInputClipboard:
@@ -340,7 +386,6 @@ begin
       fConfig.OutputSink := osClipboard;
     siOutputFile:
     begin
-      // Command is ignored if following param is not a file name
       fConfig.OutputFile := GetStringParameter(Command);
       fConfig.OutputSink := osFile;
       fParamQueue.Dequeue;
@@ -357,14 +402,17 @@ begin
     end;
     siFragment:
       fConfig.DocType := dtXHTMLFragment;
-    siHideCSSYes:
+    siForceHideCSS:
       fConfig.HideCSS := True;
-    siHideCSSNo:
-      fConfig.HideCSS := False;
     siHideCSS:
     begin
-      fConfig.HideCSS := GetBooleanParameter(Command);
-      fParamQueue.Dequeue;
+      if Command.IsSwitch then
+        fConfig.HideCSS := Command.SwitchValue
+      else
+      begin
+        fConfig.HideCSS := GetBooleanParameter(Command);
+        fParamQueue.Dequeue;
+      end;
     end;
     siEmbedCSS:
     begin
@@ -390,13 +438,14 @@ begin
     end;
     siBranding:
     begin
-      fConfig.BrandingPermitted := GetBooleanParameter(Command);
-      fParamQueue.Dequeue;
+      if Command.IsSwitch then
+        fConfig.BrandingPermitted := Command.SwitchValue
+      else
+      begin
+        fConfig.BrandingPermitted := GetBooleanParameter(Command);
+        fParamQueue.Dequeue;
+      end;
     end;
-    siBrandingYes:
-      fConfig.BrandingPermitted := True;
-    siBrandingNo:
-      fConfig.BrandingPermitted := False;
     siHelp:
       fConfig.ShowHelp := True;
     siQuiet:
@@ -422,4 +471,106 @@ begin
     fParamQueue.Enqueue(ParamStr(Idx));
 end;
 
+{ TCommand }
+
+constructor TCommand.Create(const Cmd: string);
+begin
+  ValidateCommand(Cmd);
+  fCommand := Cmd;
+end;
+
+function TCommand.GetName: string;
+begin
+  if not IsShortForm then
+    Exit(fCommand);
+  Result := AnsiLeftStr(fCommand, 2);
+end;
+
+class operator TCommand.Implicit(const S: string): TCommand;
+begin
+  ValidateCommand(S);
+  Result.fCommand := S;
+end;
+
+class operator TCommand.Implicit(const Cmd: TCommand): string;
+begin
+  Result := Cmd.Command;
+end;
+
+class function TCommand.IsCommand(const S: string): Boolean;
+var
+  Idx: Integer;
+begin
+  if AnsiStartsStr('--', S) then
+  begin
+    // long form command: '-' '-' <letter> {<letter> | '-'}
+    if Length(S) < 3 then
+      Exit(False);
+    if not TCharacter.IsLetter(S[3]) then
+      Exit(False);
+    for Idx := 4 to Length(S) do
+      if not TCharacter.IsLetter(S[Idx]) and (S[Idx] <> '-') then
+        Exit(False);
+    Result := True;
+  end
+  else if AnsiStartsStr('-', S) then
+  begin
+    // short form or legacy commands
+    if Length(S) < 2 then
+      Exit(False);
+    // short form: '-' <letter> [<switch-char>]
+    if IsShortForm(S) then
+      Exit(True);
+    // legacy:     '-' <letter> {<letter>}
+    for Idx := 2 to Length(S) do
+      if not TCharacter.IsLetter(S[Idx]) then
+        Exit(False);
+    Result := True;
+  end
+  else
+    // no preceding '-'
+    Result := False;
+end;
+
+function TCommand.IsShortForm: Boolean;
+begin
+  Result := IsShortForm(fCommand);
+end;
+
+class function TCommand.IsShortForm(const Cmd: string): Boolean;
+begin
+  if not (Length(Cmd) in [2..3]) then
+    Exit(False);
+  if Cmd[1] <> '-' then
+    Exit(False);
+  if not TCharacter.IsLetter(Cmd[2]) then
+    Exit(False);
+  if (Length(Cmd) = 3) and not CharInSet(Cmd[3], SwitchChars) then
+    Exit(False);
+  Result := True;
+end;
+
+function TCommand.IsSwitch: Boolean;
+begin
+  Result := IsShortForm and (Length(fCommand) = 3);
+end;
+
+function TCommand.SwitchValue: Boolean;
+resourcestring
+  sBadSwitch = '%s in not a switch command';
+begin
+  if not IsSwitch then
+    raise Exception.CreateFmt(sBadSwitch, [Name]);
+  Result := CharInSet(fCommand[3], TrueSwitchChars);
+end;
+
+class procedure TCommand.ValidateCommand(const Cmd: string);
+resourcestring
+  sBadCommand = '"%s" is not a valid command';
+begin
+  if not IsCommand(Cmd) then
+    raise Exception.CreateFmt(sBadCommand, [Cmd]);
+end;
+
 end.
+
