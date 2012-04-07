92e8839540f16761f279e7e2a453a3e21f924154
DelphiDabbler <git@delphidabbler.com>
2012-04-07 15:09:07 +0000

    Revised re changes in base class. Added TConfigFileWriter class that writes gui-config file.

--- Src/GUI/UGUIConfigFiles.pas
+++ Src/GUI/UGUIConfigFiles.pas
@@ -3,29 +3,72 @@ unit UGUIConfigFiles;
 interface
 
 uses
+  Classes,
   UConfigFiles;
 
 type
-  TGUIConfigFiles = class(TConfigFiles)
+  TConfigFileWriter = class(TObject)
   strict private
-    class function GUIConfigFileName: string;
+    var
+      fFileStream: TFileStream;
+  public
+    constructor Create(const FileName: string);
+    destructor Destroy; override;
+    procedure WriteLine(const Key, Value: string);
+  end;
+
+  TGUIConfigFiles = class(TConfigFiles)
   public
     class function ConfigFileReaderInstance: TConfigFileReader; override;
+    class function ConfigFileWriterInstance: TConfigFileWriter;
   end;
 
 implementation
 
+uses
+  SysUtils;
+
 { TGUIConfigFiles }
+
+const
+  GUICfgFileName = 'gui-config';
 
 class function TGUIConfigFiles.ConfigFileReaderInstance: TConfigFileReader;
 begin
-  Result := TConfigFileReader.Create;
-  Result.LoadData(GUIConfigFileName);
+  Result := inherited ConfigFileReaderInstance(GUICfgFileName);
 end;
 
-class function TGUIConfigFiles.GUIConfigFileName: string;
+class function TGUIConfigFiles.ConfigFileWriterInstance: TConfigFileWriter;
+var
+  CfgFilePath: string;
 begin
-  FindConfigFile('gui-config', Result);   // Result = '' if file doesn't exist
+  CfgFilePath := IncludeTrailingPathDelimiter(UserConfigDir) + GUICfgFileName;
+  Result := TConfigFileWriter.Create(CfgFilePath);
+end;
+
+{ TConfigFileWriter }
+
+constructor TConfigFileWriter.Create(const FileName: string);
+begin
+  inherited Create;
+  fFileStream := TFileStream.Create(FileName, fmCreate);
+end;
+
+destructor TConfigFileWriter.Destroy;
+begin
+  fFileStream.Free;
+  inherited;
+end;
+
+procedure TConfigFileWriter.WriteLine(const Key, Value: string);
+var
+  Bytes: TBytes;
+  Line: string;
+begin
+  Line := Trim(Key) + ' ' + Trim(Value) + #13#10;
+  Bytes := TEncoding.UTF8.GetBytes(Line);
+  fFileStream.Write(Pointer(Bytes)^, Length(Bytes));
 end;
 
 end.
+
