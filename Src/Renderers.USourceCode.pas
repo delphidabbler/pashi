67fe00d4a4c4a9b9721fde47ed92199fcd1416cd
delphidabbler <5164283+delphidabbler@users.noreply.github.com>
2021-09-22 11:29:48 +0100

    Update copyright date to 2021

--- Src/Renderers.USourceCode.pas
+++ Src/Renderers.USourceCode.pas
@@ -3,7 +3,7 @@
  * v. 2.0. If a copy of the MPL was not distributed with this file, You can
  * obtain one at http://mozilla.org/MPL/2.0/
  *
- * Copyright (C) 2012-2016, Peter Johnson (www.delphidabbler.com).
+ * Copyright (C) 2012-2021, Peter Johnson (www.delphidabbler.com).
  *
  * Class that highlights source code into HTML suitable for all supported
  * document types.
