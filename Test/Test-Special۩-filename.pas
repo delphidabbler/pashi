b9b4332a9a748bab96345f10fc7e0a052b4007d7
Peter Johnson <5164283+delphidabbler@users.noreply.github.com>
2022-10-27 11:30:28 +0100

    Add further test files
    New tests files to:
    - test handling of non-ANSI/ASCII characters in file names
    - test handling of non-ANSI/ASCII characters in pascal code
    - test operation of --trim command
    - test handling of UCS2LE source files with BOM.
    - test handling of UTF8 source files with BOM.
    
    Closes #67

---
+++ Test/Test-Special۩-filename.pas
@@ -0,0 +1,7 @@
+﻿// This file is used to test the handling of Unicode characters in file names.
+// It must be saved with a non-ANSI, non-ASCII character in its name.
+var
+  s: string;
+begin
+  S := 'Foo';
+end;
---
+++ Test/Test-UTF8-BOM.pas
@@ -0,0 +1,59 @@
+﻿unit IO.Readers.UStdIn;
+
+interface
+
+uses
+  SysUtils,
+  IO.UTypes;
+
+type
+  TStdInReader = class(TInterfacedObject, IInputReader)
+  strict private
+    var
+      fEncoding: TEncoding;
+  public
+    constructor Create(const Encoding: TEncoding);
+    destructor Destroy; override;
+    function Read: string;
+  end;
+
+implementation
+
+uses
+  UConsts, UStdIO;
+
+{ TStdInReader }
+
+constructor TStdInReader.Create(const Encoding: TEncoding);
+begin
+  inherited Create;
+  fEncoding := Encoding;
+end;
+
+destructor TStdInReader.Destroy;
+begin
+  if not TEncoding.IsStandardEncoding(fEncoding) then
+    fEncoding.Free;
+  inherited;
+end;
+
+function TStdInReader.Read: string;
+const
+  ChunkSize = 1024 * 16;
+var
+  Buffer: TBytes;
+  Data: TBytes;
+  BytesRead: Cardinal;
+  TotalBytes: Cardinal;
+  Offset: Cardinal;
+  S: string;
+begin
+  Result := '';
+  while not EOF do
+  begin
+    ReadLn(Input, S);
+    Result := Result + S + CRLF;
+  end;
+end;
+
+end.
